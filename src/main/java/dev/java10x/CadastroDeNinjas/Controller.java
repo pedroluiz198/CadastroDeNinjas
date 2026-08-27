ca019afcb4e616215f67187f553267133c3909bd
pedroluiz198 <pedroluizfc@gmail.com>
2026-08-27 10:50:34 -0300

    Criado a Controller [Aula 2 ]

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Essa é primeira mensagem nessa rota";
+    }
+}
